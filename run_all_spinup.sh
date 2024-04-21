2dc8f75903aef8e7e24ca9cc2c225ae3c634b557
Kang Wang <Kang.Wang@colorado.edu>
2024-04-21 16:04:02 +0800

    Create run_all_spinup.sh

---
+++ run_all_spinup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+#
+# Check ats version ...
+
+ats --version
+
+arr0=( "02peat" "05peat" )
+arr1=( "cd" "cw" "wd" "ww")
+
+for i1 in "${arr1[@]}"
+do
+for i0 in "${arr0[@]}"
+do
+cd /Users/kangwang/Documents/GitHub/warming-or-cooling
+cd regions/$i1/$i0/spinup
+echo regions/$i1/$i0/spinup
+./run.sh &
+done
+done
+
+sleep 5
