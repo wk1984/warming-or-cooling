1bb6fd484a9119b96226bad2f0967ad8593c19b7
Kang Wang <Kang.Wang@colorado.edu>
2024-04-21 16:04:06 +0800

    Create run_all_control.sh

---
+++ run_all_control.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+#
+# Check ats version ...
+
+ats --version
+
+arr0=( "02peat" "05peat" )
+arr1=( "cd" "cw" "wd" "ww")
+
+for i1 in "${arr1[@]}"
+do
+for i0 in "${arr0[@]}"
+do
+cd /Users/kangwang/Documents/GitHub/warming-or-cooling
+cd regions/$i1/$i0/c
+echo regions/$i1/$i0/c
+./run.sh &
+done
+done
+
+sleep 5
