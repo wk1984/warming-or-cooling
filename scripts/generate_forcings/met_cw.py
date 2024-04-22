a1c6834de2f4d8890999dcaaa5afeb35e1caa47d
Kang Wang <Kang.Wang@colorado.edu>
2024-04-22 16:52:25 +0800

    update forcing scripts

--- scripts/generate_forcings/met_cw.py
+++ scripts/generate_forcings/met_cw.py
@@ -102,62 +102,62 @@ with h5py.File(basepath + '/data/cw_control.h5', 'w') as hf:
     hf.create_dataset('wind speed [m s^-1]', data=ws)
 hf.close()
 
-with h5py.File(basepath + '/data/cw_irrigation_multi.h5', 'w') as hf:
-    hf.create_dataset('time [s]', data=tim)
-    hf.create_dataset('air temperature [K]', data=t+273.15)
-    hf.create_dataset('incoming shortwave radiation [W m^-2]', data=swin)
-    hf.create_dataset('precipitation rain [m s^-1]', data=rr_i_multi/86400000)
-    hf.create_dataset('precipitation snow [m SWE s^-1]', data=sn/86400000)
-    hf.create_dataset('relative humidity [-]', data=rh)
-    hf.create_dataset('wind speed [m s^-1]', data=ws)
-hf.close()
-
-with h5py.File(basepath + '/data/cw_irrigation_50more.h5', 'w') as hf:
-    hf.create_dataset('time [s]', data=tim)
-    hf.create_dataset('air temperature [K]', data=t+273.15)
-    hf.create_dataset('incoming shortwave radiation [W m^-2]', data=swin)
-    hf.create_dataset('precipitation rain [m s^-1]', data=rr_i_50more/86400000)
-    hf.create_dataset('precipitation snow [m SWE s^-1]', data=sn/86400000)
-    hf.create_dataset('relative humidity [-]', data=rh)
-    hf.create_dataset('wind speed [m s^-1]', data=ws)
-hf.close()
-
-with h5py.File(basepath + '/data/cw_irrigation_50less.h5', 'w') as hf:
-    hf.create_dataset('time [s]', data=tim)
-    hf.create_dataset('air temperature [K]', data=t+273.15)
-    hf.create_dataset('incoming shortwave radiation [W m^-2]', data=swin)
-    hf.create_dataset('precipitation rain [m s^-1]', data=rr_i_50less/86400000)
-    hf.create_dataset('precipitation snow [m SWE s^-1]', data=sn/86400000)
-    hf.create_dataset('relative humidity [-]', data=rh)
-    hf.create_dataset('wind speed [m s^-1]', data=ws)
-hf.close()
-
-with h5py.File(basepath + '/data/cw_irrigation_multi4.h5', 'w') as hf:
-    hf.create_dataset('time [s]', data=tim)
-    hf.create_dataset('air temperature [K]', data=t+273.15)
-    hf.create_dataset('incoming shortwave radiation [W m^-2]', data=swin)
-    hf.create_dataset('precipitation rain [m s^-1]', data=rr_i_multi4/86400000)
-    hf.create_dataset('precipitation snow [m SWE s^-1]', data=sn/86400000)
-    hf.create_dataset('relative humidity [-]', data=rh)
-    hf.create_dataset('wind speed [m s^-1]', data=ws)
-hf.close()
-#%%
-
-rain_i = rain.copy()
-rain_i = rain_i*1.5
-rr_c = repeat_cycle(rain, 14)
-
-rr = repeat_cycle(rain, 10)
-rr_i = np.concatenate((rr, rain_i, rain, rain, rain))
-
-#%%
-tim = np.arange(0.0, 86400 * 365 * 14, 86400)
-with h5py.File(basepath + '/data/cw_irrigation_grad.h5', 'w') as hf:
-    hf.create_dataset('time [s]', data=tim)
-    hf.create_dataset('air temperature [K]', data=t+273.15)
-    hf.create_dataset('incoming shortwave radiation [W m^-2]', data=swin)
-    hf.create_dataset('precipitation rain [m s^-1]', data=rr_i/86400000)
-    hf.create_dataset('precipitation snow [m SWE s^-1]', data=sn/86400000)
-    hf.create_dataset('relative humidity [-]', data=rh)
-    hf.create_dataset('wind speed [m s^-1]', data=ws)
-hf.close()
+# with h5py.File(basepath + '/data/cw_irrigation_multi.h5', 'w') as hf:
+#     hf.create_dataset('time [s]', data=tim)
+#     hf.create_dataset('air temperature [K]', data=t+273.15)
+#     hf.create_dataset('incoming shortwave radiation [W m^-2]', data=swin)
+#     hf.create_dataset('precipitation rain [m s^-1]', data=rr_i_multi/86400000)
+#     hf.create_dataset('precipitation snow [m SWE s^-1]', data=sn/86400000)
+#     hf.create_dataset('relative humidity [-]', data=rh)
+#     hf.create_dataset('wind speed [m s^-1]', data=ws)
+# hf.close()
+# 
+# with h5py.File(basepath + '/data/cw_irrigation_50more.h5', 'w') as hf:
+#     hf.create_dataset('time [s]', data=tim)
+#     hf.create_dataset('air temperature [K]', data=t+273.15)
+#     hf.create_dataset('incoming shortwave radiation [W m^-2]', data=swin)
+#     hf.create_dataset('precipitation rain [m s^-1]', data=rr_i_50more/86400000)
+#     hf.create_dataset('precipitation snow [m SWE s^-1]', data=sn/86400000)
+#     hf.create_dataset('relative humidity [-]', data=rh)
+#     hf.create_dataset('wind speed [m s^-1]', data=ws)
+# hf.close()
+# 
+# with h5py.File(basepath + '/data/cw_irrigation_50less.h5', 'w') as hf:
+#     hf.create_dataset('time [s]', data=tim)
+#     hf.create_dataset('air temperature [K]', data=t+273.15)
+#     hf.create_dataset('incoming shortwave radiation [W m^-2]', data=swin)
+#     hf.create_dataset('precipitation rain [m s^-1]', data=rr_i_50less/86400000)
+#     hf.create_dataset('precipitation snow [m SWE s^-1]', data=sn/86400000)
+#     hf.create_dataset('relative humidity [-]', data=rh)
+#     hf.create_dataset('wind speed [m s^-1]', data=ws)
+# hf.close()
+# 
+# with h5py.File(basepath + '/data/cw_irrigation_multi4.h5', 'w') as hf:
+#     hf.create_dataset('time [s]', data=tim)
+#     hf.create_dataset('air temperature [K]', data=t+273.15)
+#     hf.create_dataset('incoming shortwave radiation [W m^-2]', data=swin)
+#     hf.create_dataset('precipitation rain [m s^-1]', data=rr_i_multi4/86400000)
+#     hf.create_dataset('precipitation snow [m SWE s^-1]', data=sn/86400000)
+#     hf.create_dataset('relative humidity [-]', data=rh)
+#     hf.create_dataset('wind speed [m s^-1]', data=ws)
+# hf.close()
+# #%%
+# 
+# rain_i = rain.copy()
+# rain_i = rain_i*1.5
+# rr_c = repeat_cycle(rain, 14)
+# 
+# rr = repeat_cycle(rain, 10)
+# rr_i = np.concatenate((rr, rain_i, rain, rain, rain))
+# 
+# #%%
+# tim = np.arange(0.0, 86400 * 365 * 14, 86400)
+# with h5py.File(basepath + '/data/cw_irrigation_grad.h5', 'w') as hf:
+#     hf.create_dataset('time [s]', data=tim)
+#     hf.create_dataset('air temperature [K]', data=t+273.15)
+#     hf.create_dataset('incoming shortwave radiation [W m^-2]', data=swin)
+#     hf.create_dataset('precipitation rain [m s^-1]', data=rr_i/86400000)
+#     hf.create_dataset('precipitation snow [m SWE s^-1]', data=sn/86400000)
+#     hf.create_dataset('relative humidity [-]', data=rh)
+#     hf.create_dataset('wind speed [m s^-1]', data=ws)
+# hf.close()
